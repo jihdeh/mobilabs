60d2570ca9dc62e9aebdf644ecd0fd6cbcae02df
Jihdeh <jide.b.tade@gmail.com>
2016-11-12 16:36:47 +0100

    rm etag for deebuggering

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ function App() {
   app
     .use(responseTime())
     .use(logger())
-    .use(etag())
+    // .use(etag())
     .use(mount("/api", Api()))
     .use(mount("/", Frontend()));
   return app;
